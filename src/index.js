df8980689a0d5504a57ba0c30643025157fb39a6
LizaAqua <148656169+LizaAqua@users.noreply.github.com>
2024-11-27 18:46:34 +0000

    *

--- src/index.js
+++ src/index.js
@@ -62,8 +62,8 @@ function formatDay(timestamp) {
 }
 
 function getForecast(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "abfec4b8be01abt65a50c3e50o5aa3d7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=abfec4b8be01abt65a50c3e50o5aa3d7&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
 
