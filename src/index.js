f059a7fc2af98edd54af7ceeeabffdd58e18ae69
LizaAqua <148656169+LizaAqua@users.noreply.github.com>
2024-11-29 15:56:19 +0000

    *

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "abfec4b8be01abt65a50c3e50o5aa3d7";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
@@ -63,7 +63,7 @@ function formatDay(timestamp) {
 
 function getForecast(city) {
   let apiKey = "abfec4b8be01abt65a50c3e50o5aa3d7";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query={query}&key={key}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
 
